86b6cabbcf5731b12d8fdfd11516e98db141ceef
Awowz <tonygriffith75@gmail.com>
2025-01-12 22:10:06 -0600

    Added empty circleshape.py and additional lines in main.py

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import pygame
 from constants import *
 
-def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
+def main():
+    pygame.init()
+    clock_object = pygame.time.Clock()
+    delta_time = 0 ## amount of time passed since last frame was drawn
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        
+
+
+        # RENDER
+        screen.fill((35,43,43))
+        # the following command should be the last line for displaying
+        pygame.display.flip()
+
+
+        ##after the main gameloop has run run tick
+        delta_time = clock_object.tick(60) / 1000 
 
 
 if __name__ == "__main__":
