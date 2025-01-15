4bc918efcac94e31d86052dbab9cd1fee00885ae
Awowz <tonygriffith75@gmail.com>
2025-01-14 21:39:43 -0600

    added asteroids asteroidfield and updated main and player

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -8,7 +11,18 @@ def main():
     delta_time = 0 ## amount of time passed since last frame was drawn
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    
+  
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    all_asteroids = pygame.sprite.Group()
+
+    # note: player must be created after asigning static field, otherwise existing object wont take effect
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (all_asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    my_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)   
+    AsteroidField_object = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,14 +31,29 @@ def main():
         
 
 
+        ##update
+        #my_player.update(delta_time)
+        for update_object in updatable:
+            update_object.update(delta_time)
+
+
         # RENDER
+        # fill screen first
         screen.fill((35,43,43))
+
+        #draw player sometime after fill but not after flip()
+        #my_player.draw(screen)
+        for draw_object in drawable:
+            draw_object.draw(screen)
+
         # the following command should be the last line for displaying
         pygame.display.flip()
 
 
         ##after the main gameloop has run run tick
         delta_time = clock_object.tick(60) / 1000 
+
+
 
 
 if __name__ == "__main__":
