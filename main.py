2876da87ad3e570e07f6062ed44e6c0e9f518f5e
Awowz <tonygriffith75@gmail.com>
2025-01-16 19:28:52 -0600

    cleaned up main.py adding more functions

--- main.py
+++ main.py
@@ -8,12 +8,31 @@ from shot import Shot
 from particle import Particle
 
 
+def render_game_objects(screen, drawable):
+    screen.fill((35,43,43))
+
+    for draw_object in drawable:
+        draw_object.draw(screen)
+
+    # the following command should be the last line for rendering
+    pygame.display.flip()
+
+
+def update_game_logic(delta_time, my_player, updatable, all_asteroids):
+    for update_object in updatable:
+        update_object.update(delta_time)
+
+    for single_asteroid in all_asteroids:     #collision check
+        if single_asteroid.checkCollision(my_player):
+            print("GAME OVER!")
+            sys.exit()
+    
+
 def main():
     pygame.init()
     clock_object = pygame.time.Clock()
     delta_time = 0 ## amount of time passed since last frame was drawn
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
   
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -29,41 +48,19 @@ def main():
 
     my_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  
     AsteroidField_object = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        
+        update_game_logic(delta_time, my_player, updatable, all_asteroids)    
 
-
-        ##update
-        #my_player.update(delta_time)
-        for update_object in updatable:
-            update_object.update(delta_time)
-
-        #collision check
-        for single_asteroid in all_asteroids:
-            if single_asteroid.checkCollision(my_player):
-                print("GAME OVER!")
-                sys.exit()
-
-        # RENDER
-        # fill screen first
-        screen.fill((35,43,43))
-
-        #draw player sometime after fill but not after flip()
-        #my_player.draw(screen)
-        for draw_object in drawable:
-            draw_object.draw(screen)
-
-        # the following command should be the last line for displaying
-        pygame.display.flip()
-
+        render_game_objects(screen, drawable)
 
         ##after the main gameloop has run run tick
         delta_time = clock_object.tick(60) / 1000 
-
+        
 
 
 
