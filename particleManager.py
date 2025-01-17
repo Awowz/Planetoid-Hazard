41d1c35044a8c9ff60ac2e59f11820103ae95847
Awowz <tonygriffith75@gmail.com>
2025-01-16 20:04:37 -0600

    added slight deviation in particles

--- particleManager.py
+++ particleManager.py
@@ -1,7 +1,7 @@
 import pygame
+import random
 from particle import Particle
 from constants import * 
-
 
 class ParticleManager():
     def __init__(self):
@@ -12,10 +12,10 @@ class ParticleManager():
         self.particle_list = []
 
 
-    def create_standard_particle(self, position, quantity, velocity):
+    def create_standard_particle(self, position, quantity, velocity, degree_variance=0):
         for x in range(quantity):
-            temp = Particle(position.x, position.y, PARTICLE_RADIUS, 2)
-            temp.velocity = velocity
+            temp = Particle(position.x, position.y, PARTICLE_RADIUS, 1)
+            temp.velocity = velocity.rotate(random.randint(-degree_variance,degree_variance))
             temp.deceleration = -velocity / temp.fade_time
 
     def create_particle_thrust(self):
