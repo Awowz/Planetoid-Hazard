c1ded07301323e510d2805ca6a7fcc02d1145691
Awowz <tonygriffith75@gmail.com>
2025-01-19 14:03:55 -0600

    optimized shots to despawn after leaving kill border

--- shot.py
+++ shot.py
@@ -11,3 +11,8 @@ class Shot(CircleShape):
     
     def update(self, delta_time):
         self.position += self.velocity * delta_time
+        
+        if self.position.x <= -BORDER_KILL_ZONE_X or BORDER_KILL_ZONE_X + SCREEN_WIDTH < self.position.x:
+            self.kill()
+        if self.position.y <= -BORDER_KILL_ZONE_Y or BORDER_KILL_ZONE_Y + SCREEN_HEIGHT < self.position.y:
+            self.kill()
